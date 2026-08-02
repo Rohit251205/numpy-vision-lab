d24a8a7a9654f3cfb0bd31a196fb25f8fde569c8
Rohit Prajapati <pr144599@gmail.com>
2026-08-02 18:37:11 +0530

    Added image loading and information module

---
+++ README.md
@@ -0,0 +1,20 @@
+# NumPy Vision Lab
+
+## Overview
+
+NumPy Vision Lab is a lightweight image processing project built using NumPy and Pillow.
+
+The project demonstrates how images can be represented as NumPy arrays and processed using basic image transformations.
+
+## Technologies
+
+- Python
+- NumPy
+- Pillow
+- Git
+- GitHub
+- VS Code
+
+## Project Status
+
+🚧 In Development
---
+++ main.py
@@ -0,0 +1,51 @@
+"""
+==========================================================
+Project : NumPy Vision Lab
+Version : 1.0
+
+Author : Rohit Prajapati
+
+Description:
+A lightweight image processing toolkit using NumPy and Pillow.
+==========================================================
+"""
+
+# ==============================
+# Import Libraries
+# ==============================
+
+import numpy as np
+from PIL import Image
+
+# ==============================
+# Main Code
+# ==============================
+
+# Image Loading
+
+car_img = Image.open("images/car.jpeg")
+
+# Convert Image In Numpy Array
+
+car_array = np.array(car_img)
+
+# =========================================
+# IMAGE INFORMATION
+# =========================================
+
+image_shape = car_array.shape
+image_height = image_shape[0]
+image_width = image_shape[1]
+image_channels = image_shape[2]
+
+
+print("Image Loaded Successfully \n")
+print("Image Name :",car_img.filename)
+print("Image Shape :",image_shape)
+print("Image Height :",image_height)
+print("Image Width :",image_width)
+print("Image Channels :",image_channels)
+print("Image Size :",car_array.size)
+print("Image Datatype :",car_array.dtype)
+print("Minimum Pixel :",np.min(car_array))
+print("Maximum Pixel :",np.max(car_array))
